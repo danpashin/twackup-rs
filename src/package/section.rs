a8329d112ca2090fcfad0d3a4ffe690017f0e30f
Daniil Pashin <danypashin@yandex.ru>
2020-11-22 11:01:16 +0300

    [*] Split package model module

---
+++ src/package/section.rs
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 DanP
+ *
+ * This file is part of Twackup
+ *
+ * Twackup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Twackup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Twackup. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#[derive(Clone, PartialEq)]
+pub enum Section {
+    Other(String),
+    Unknown,
+    System,
+    Tweaks,
+    Utilities,
+    Packaging,
+    Development,
+    TerminalSupport,
+    Themes,
+    Archiving,
+    Networking,
+    TextEditors,
+}
+
+impl Section {
+    pub fn from_string(value: &str) -> Section {
+        match value.to_lowercase().as_str() {
+            "system" => Section::System,
+            "tweaks" => Section::Tweaks,
+            "utilities" => Section::Utilities,
+            "packaging" => Section::Packaging,
+            "development" => Section::Development,
+            "themes" => Section::Themes,
+            "terminal_support" | "terminal support" => Section::TerminalSupport,
+            "networking" => Section::Networking,
+            "archiving" => Section::Archiving,
+            "text_editors" => Section::TextEditors,
+            _ => Section::Other(value.to_string())
+        }
+    }
+
+    pub fn from_string_opt(value: Option<&String>) -> Section {
+        match value {
+            Some(value) => Self::from_string(value),
+            None => Section::Unknown
+        }
+    }
+}
---
+++ src/package/state.rs
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 DanP
+ *
+ * This file is part of Twackup
+ *
+ * Twackup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Twackup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Twackup. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#[derive(Clone, PartialEq)]
+pub enum State {
+    Unknown,
+    Install,
+    Remove,
+    Hold,
+}
+
+impl State {
+    pub fn from_dpkg(string: Option<&String>) -> Self {
+        if let Some(status) = string {
+            let mut components = status.split_whitespace();
+            if let Some(state) = components.next() {
+                return match state.to_lowercase().as_str() {
+                    "install" => Self::Install,
+                    "deinstall" => Self::Remove,
+                    "hold" => Self::Hold,
+                    _ => Self::Unknown
+                };
+            }
+        }
+
+        return Self::Unknown;
+    }
+}
