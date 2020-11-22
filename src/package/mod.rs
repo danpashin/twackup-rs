135621af1c7920fa53ff1231cfb5a0eb5d843824
Daniil Pashin <danypashin@yandex.ru>
2020-11-22 17:53:13 +0300

    [*] Optimize memory usage by replacing package mode fields keys to enum values

--- src/package/mod.rs
+++ src/package/mod.rs
@@ -20,15 +20,17 @@
 mod state;
 mod section;
 mod priority;
+mod field;
 
 use std::{
     collections::HashMap,
     fs::File, path::Path,
     io::{self, BufReader, BufRead},
+    str::FromStr,
 };
 use crate::kvparser::Parsable;
 pub use self::{
-    state::State, section::Section, priority::Priority,
+    state::State, section::Section, priority::Priority, field::Field,
 };
 
 #[derive(Clone)]
@@ -43,9 +45,6 @@ pub struct Package {
     /// Version of package. This field MUST NOT be empty.
     pub version: String,
 
-    /// Architecture of package. This field MUST NOT be empty.
-    pub architecture: String,
-
     /// State of package as it was marked by dpkg itself.
     /// If this field is empty, Unknown state must be used.
     pub state: State,
@@ -56,14 +55,19 @@ pub struct Package {
 
     pub priority: Priority,
 
-    fields: HashMap<String, String>,
+    fields: HashMap<Field, String>,
 }
 
 impl Parsable for Package {
     type Output = Self;
 
     fn new(fields: HashMap<String, String>) -> Option<Self::Output> {
-        let package_id = fields.get("Package")?;
+        let fields: HashMap<Field, String> = fields.into_iter().map(|(key, value)| {
+            // Safe to unwrap because from_str doesn't return error
+            (Field::from_str(key.as_str()).unwrap(), value)
+        }).collect();
+
+        let package_id = fields.get(&Field::Package)?;
         // Ignore virtual packages
         if package_id.starts_with("gsc.") || package_id.starts_with("cy+") {
             return None;
@@ -71,12 +75,11 @@ impl Parsable for Package {
 
         Some(Self {
             id: package_id.to_string(),
-            name: fields.get("Name").unwrap_or(package_id).to_string(),
-            version: fields.get("Version")?.to_string(),
-            architecture: fields.get("Architecture")?.to_string(),
-            state: State::from_dpkg(fields.get("Status")),
-            section: Section::from_string_opt(fields.get("Section")),
-            priority: Priority::from_string_opt(fields.get("Priority")),
+            name: fields.get(&Field::Name).unwrap_or(package_id).to_string(),
+            version: fields.get(&Field::Version)?.to_string(),
+            state: State::from_dpkg(fields.get(&Field::Status)),
+            section: Section::from_string_opt(fields.get(&Field::Section)),
+            priority: Priority::from_string_opt(fields.get(&Field::Priority)),
             fields,
         })
     }
@@ -92,22 +95,24 @@ impl Package {
     /// Creates canonical DEB filename in format of id_version_arch
     #[inline]
     pub fn canonical_name(&self) -> String {
-        format!("{}_{}_{}", self.id, self.version, self.architecture)
+        let arch = self.fields.get(&Field::Architecture).unwrap_or(&String::new()).to_string();
+        format!("{}_{}_{}", self.id, self.version, arch)
     }
 
     /// Converts model to DEB control file
     pub fn to_control(&self) -> String {
         let mut fields_len = 0;
         for (key, value) in self.fields.iter() {
-            fields_len += key.len() + value.len() + 3;
+            if *key == Field::Status { continue; }
+            fields_len += key.as_str().len() + value.len() + 3;
         }
 
         let mut control = String::with_capacity(fields_len);
 
         for (key, value) in self.fields.iter() {
             // Skip status field. It is invalid in control
-            if key.as_str() == "Status" { continue; }
-            control.push_str(key);
+            if *key == Field::Status { continue; }
+            control.push_str(key.as_str());
             control.push_str(": ");
             control.push_str(value);
             control.push_str("\n");
@@ -117,7 +122,6 @@ impl Package {
     }
 
     /// Splits optional string by comma to vector of strings
-    #[inline]
     fn split_by_comma(&self, field: Option<&String>) -> Vec<String> {
         match field {
             None => Vec::new(),
@@ -127,11 +131,11 @@ impl Package {
 
     /// Returns packages of which this one depends.
     #[inline]
-    pub fn depends(&self) -> Vec<String> { self.split_by_comma(self.fields.get("Depends")) }
+    pub fn depends(&self) -> Vec<String> { self.split_by_comma(self.fields.get(&Field::Depends)) }
 
     /// Returns packages of which the installation of this package depends.
     #[inline]
-    pub fn predepends(&self) -> Vec<String> { self.split_by_comma(self.fields.get("Pre-Depends")) }
+    pub fn predepends(&self) -> Vec<String> { self.split_by_comma(self.fields.get(&Field::PreDepends)) }
 
     /// Returns true if this package us a dependency of other.
     pub fn is_dependency_of(&self, pkg: &Package) -> bool {
@@ -140,7 +144,8 @@ impl Package {
     }
 
     #[allow(dead_code)]
-    pub fn get_field<F: AsRef<str>>(&self, field: F) -> Option<&String> {
-        self.fields.get(field.as_ref())
+    #[inline]
+    pub fn get_field(&self, field: &Field) -> Option<&String> {
+        self.fields.get(field)
     }
 }
