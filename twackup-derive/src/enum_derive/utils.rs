b0a64404bb8312e437e8c1a39d8018ed0aba249d
Dan Pashin <danypashin@yandex.ru>
2023-04-21 20:43:03 +0300

    Update syn

--- twackup-derive/src/enum_derive/utils.rs
+++ twackup-derive/src/enum_derive/utils.rs
@@ -18,8 +18,7 @@
  */
 
 use convert_case::{Case, Casing};
-use quote::ToTokens;
-use syn::{Attribute, DeriveInput, Lit, Meta, NestedMeta, Variant};
+use syn::{punctuated::Punctuated, token::Comma, Attribute, DeriveInput, Expr, Lit, Meta, Variant};
 
 pub(crate) const ATTRIBUTE_ROOT_NAME: &str = "twackup";
 pub(crate) const ATTRIBUTE_RENAME: &str = "rename";
@@ -29,11 +28,9 @@ pub(crate) const ATTRIBUTE_CONVERT_ALL: &str = "convert_all";
 pub(crate) fn enum_field_name(variant: &Variant, convert_form: Option<&String>) -> String {
     let meta = twackup_attributes_meta(&variant.attrs);
     let name = meta.iter().find_map(|meta| match meta {
-        NestedMeta::Meta(Meta::NameValue(m)) if m.path.is_ident(ATTRIBUTE_RENAME) => {
-            Some(lit_get_string(&m.lit))
-        }
-        NestedMeta::Meta(Meta::NameValue(m)) if m.path.is_ident(ATTRIBUTE_CONVERT) => {
-            let value = lit_get_string(&m.lit);
+        Meta::NameValue(m) if m.path.is_ident(ATTRIBUTE_RENAME) => Some(expr_get_string(&m.value)),
+        Meta::NameValue(m) if m.path.is_ident(ATTRIBUTE_CONVERT) => {
+            let value = expr_get_string(&m.value);
             let to_return = variant.ident.to_string();
             Some(convert_string(to_return, Some(value.as_str())))
         }
@@ -61,42 +58,38 @@ pub(crate) fn convert_string(string: String, form: Option<&str>) -> String {
     converted.split_whitespace().collect()
 }
 
-pub(crate) fn twackup_attributes_meta(attributes: &[Attribute]) -> Vec<NestedMeta> {
+pub(crate) fn twackup_attributes_meta(attributes: &[Attribute]) -> Punctuated<Meta, Comma> {
     attributes
         .iter()
         .flat_map(|attribute| {
-            if !attribute.path.is_ident(ATTRIBUTE_ROOT_NAME) {
-                return vec![];
+            if !attribute.path().is_ident(ATTRIBUTE_ROOT_NAME) {
+                return Punctuated::<Meta, Comma>::new();
             }
 
-            match attribute.parse_meta() {
-                Ok(Meta::List(meta)) => meta.nested.into_iter().collect(),
-                Ok(Meta::NameValue(name_val)) => panic!(
-                    "Attribute string is not well-formed, got {:?}",
-                    name_val.to_token_stream().to_string()
-                ),
-                Ok(Meta::Path(path)) => panic!(
-                    "Attribute string is not well-formed, got {:?}",
-                    path.to_token_stream().to_string()
-                ),
-                Err(error) => panic!("{} at {:?}", error, attribute.to_token_stream().to_string()),
-            }
+            attribute
+                .parse_args_with(Punctuated::parse_terminated)
+                .unwrap_or_default()
         })
         .collect()
 }
 
-pub(crate) fn lit_get_string(lit: &Lit) -> String {
-    match lit {
-        Lit::Str(lit) => lit.value(),
-        _ => panic!("Value is not a string!"),
-    }
+pub(crate) fn expr_get_string(expr: &Expr) -> String {
+    let Expr::Lit(lit_expr) = expr else {
+        panic!("Expresssion is not a lit expression!");
+    };
+
+    let Lit::Str(string) = &lit_expr.lit else {
+        panic!("Value is not a string!");
+    };
+
+    string.value()
 }
 
 pub(crate) fn get_convert_all_attr(input: &DeriveInput) -> Option<String> {
     let meta = twackup_attributes_meta(&input.attrs);
     meta.iter().find_map(|meta| match meta {
-        NestedMeta::Meta(Meta::NameValue(m)) if m.path.is_ident(ATTRIBUTE_CONVERT_ALL) => {
-            Some(lit_get_string(&m.lit))
+        Meta::NameValue(m) if m.path.is_ident(ATTRIBUTE_CONVERT_ALL) => {
+            Some(expr_get_string(&m.value))
         }
         _ => None,
     })
